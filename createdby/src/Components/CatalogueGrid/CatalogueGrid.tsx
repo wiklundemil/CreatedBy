1cbd8d0536d3ad8021b081a9b89df28ac99f2b8a
wiklundemil <wiklund.emil2000@hotmail.com>
2024-10-24 18:12:24 +0200

    Fixed a error regarding projects not showing up in grid of projects.

--- createdby/src/Components/CatalogueGrid/CatalogueGrid.tsx
+++ createdby/src/Components/CatalogueGrid/CatalogueGrid.tsx
@@ -10,22 +10,27 @@ interface Props {
 }
 
 const CatalogueGrid: React.FC<Props> = ({
-    catalogueObjects, onItemClick,
+    catalogueObjects,
+    onItemClick,
 }) => {
-    const objects = catalogueObjects.map((item, index) => (
-        <Grid item xs={2} sm={4} md={4} key={index}>
-            <CatalogueItem
-                title={item.title}
-                onClick={() => onItemClick(item)} // Trigger the callback when clicked
-            />
-        </Grid>
-    ));
+    const objects: JSX.Element[] = [];
+
+    for (let index = 0; index < catalogueObjects.length; index++) {
+        const item = catalogueObjects[index];
+        objects.push(
+            <Grid item xs={2} sm={4} md={4} key={index}>
+                <CatalogueItem
+                    title={item.title}
+                    onClick={() => onItemClick(item)} //Trigger callback
+                />
+            </Grid>
+        );
+    }
 
     return (
-
         <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-           <Grid item xs={12} style={{ marginBottom: '20px'}}>
-                <Banner></Banner>
+            <Grid item xs={12} style={{ marginBottom: '20px' }}>
+                <Banner />
             </Grid>
             {objects}
         </Grid>
